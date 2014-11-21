67e02cedbc6579d003f6ac786b0c36d7acdcfa45
xu <584657555@qq.com>
2014-11-21 20:34:46 +0800

    修改代码

--- src/main/java/org/testng/test/JDlogin.java
+++ src/main/java/org/testng/test/JDlogin.java
@@ -1,4 +1,4 @@
-package org.testng.test;
+﻿package org.testng.test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,7 +24,7 @@ public class JDlogin
   @BeforeMethod
   public void beforeMethod() 
   {
-	  System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	  System.setProperty("webdriver.chrome.driver", "D:\\eclipsework\\test\\driver\\chromedriver.exe");
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
 	  
