9b595f67b215e1215f40a933e138295680b52107
xuguanzong <584657555@qq.com>
2014-11-24 19:48:22 +0800

    Update JDlogin.java

--- src/main/java/org/testng/test/JDlogin.java
+++ src/main/java/org/testng/test/JDlogin.java
@@ -40,7 +40,7 @@ public class JDlogin
 	  LogIn_Function loginpage = PageFactory.initElements(driver, LogIn_Function.class);
 	  loginpage.loginlink.click();
 	  Thread.sleep(3000);
-	  loginpage.LogIn_Action("584657555@qq.com", "cfadhp200906");
+	  loginpage.LogIn_Action("584657555@qq.com", "cfadhp20090622");
 	  Thread.sleep(3000);
 	  Assert.assertEquals("[退出]", loginpage.logoutlink.getText());
 	  
